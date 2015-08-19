c2be281e349ff42fc872d28937a371ab0cd80423
Joseph Dykstra <josephdykstra@gmail.com>
2015-08-19 11:20:03 -0500

    Add a boilerplate file

---
+++ -template.js
@@ -0,0 +1,10 @@
+// Joseph Dykstra
+// [date of solution]
+
+// [Name of exercise]
+// http://programmingpraxis.com/[link to exercise]
+
+// [80 character wrapped exercise description...
+// ...which extends over multiple lines]
+
+code goes here
